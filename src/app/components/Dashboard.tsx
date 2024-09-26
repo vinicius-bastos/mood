ef59d01140540ad05f1db98b1570dc5a6a083c32
vinicius.cbastos <vinicius.cbastos@aec.com.br>
2024-09-26 01:36:54 -0300

    adding moods into Dashboard page

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -11,6 +11,14 @@ type Statuses = {
   date: string
 }
 
+type Moods = {
+  "&*@#$": string
+  Sad: string
+  Existing: string
+  Good: string
+  Elated: string
+}
+
 export default function Dashboard() {
 
   const statuses = {
@@ -19,21 +27,43 @@ export default function Dashboard() {
     date: (new Date()).toDateString(),
   }
 
+  const moods = {
+    "&*@#$": "😭",
+    "Sad": "😢",
+    "Existing": "😶",
+    "Good": "😊",
+    "Elated": "😍",
+  }
+
   return (
-    <div className="flex flex-col flex-1 gap-10">
-      <div className="grid grid-cols-1 sm:grid-cols-3 bg-indigo-50
-      text-indigo-500 rounded-lg">
+    <div className="flex flex-col flex-1 gap-8 sm:gap-12 md:gap-16">
+      <div className="grid grid-cols-3 bg-indigo-50
+      text-indigo-500 p-4 gap-4 rounded-lg">
         {(Object.keys(statuses) as Array<keyof Statuses>).map((status, statusIndex) => {
           return (
-            <div key={statusIndex} className="p-4 flex flex-col gap-1
+            <div key={statusIndex} className="flex flex-col gap-1
             sm:gap-2" >
-              <p className="font-medium uppercase text-xs sm:text-sm">
+              <p className="font-medium uppercase text-xs sm:text-sm truncate">
                 {status.replaceAll("_", " ")}
               </p>
-              <p className={`text-base sm:text-lg ${fugaz.className}`}>
+              <p className={`text-base sm:text-lg truncate ${fugaz.className}`}>
                 {statuses[status]}
               </p>
             </div>
+          )
+        })}
+      </div>
+      <h4 className={`text-5xl sm:text-6xl md:text-7xl text-center ${fugaz.className}`}>
+        How do you <span className="textGradient">fell</span> today?
+      </h4>
+      <div className="grid grid-cols-2 md:grid-col-5 gap-4">
+        {(Object.keys(moods) as Array<keyof Moods>).map((mood, moodIndex) => {
+          return (
+            <button className={`${moodIndex === Object.keys(moods).length -1
+            ? "col-span-2" : "" }`} key={moodIndex}>
+              <p>{mood}</p>
+              <p>{moods[mood]}</p>
+            </button>
           )
         })}
       </div>
